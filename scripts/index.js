8f0bb27c6248c2b20534e334f676ab21db8d532d
Lvova Alina <lvova.aline@gmail.com>
2022-12-20 17:17:45 +0400

    feture второй способ добавления элемента

--- scripts/index.js
+++ scripts/index.js
@@ -33,8 +33,8 @@ const initialCards = [
   ];
 
 
-let titleInput = document.querySelector('#input-title');
-let linkInput = document.querySelector('#input-link');
+let titleImgInput = document.querySelector('#input-title');
+let linkImgInput = document.querySelector('#input-link');
 
 let nameInput = document.querySelector('#input-name');
 let descrptInput = document.querySelector('#input-descrpt');
@@ -76,11 +76,15 @@ function saveInputEditForm (evt) {
 
 function saveInputAddForm (evt) {
     evt.preventDefault();
-    // descrptProfile.textContent = titleInput.value; 
-    // nameProfile.textContent =  linkInput.value; 
+    let newCard = {};
+    newCard.name = titleImgInput.value;
+    newCard.link = linkImgInput.value;
+    initialCards.push({name: titleImgInput.value, link: linkImgInput.value});
+    console.log(initialCards);
     closePopup(popupAddForm);
-    titleInput.value="";
-    linkInput.value="";
+    renderCardList(titleImgInput.value, linkImgInput.value);
+    titleImgInput.value="";
+    linkImgInput.value="";
 }
 
 popupEditForm.addEventListener('submit', saveInputEditForm);
@@ -90,32 +94,23 @@ popupAddForm.addEventListener('submit', saveInputAddForm);
 const cardsList = document.querySelector('.gallery__list');
 
 const createCard = (nameCard, linkCard) => {
-    const li = document.createElement('li');
-    const article = document.createElement('article');
-    const img =  document.createElement('img');
-    const div = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const buttonLike = document.createElement('button');
-
-    li.classList.add('card');
-    img.classList.add('card__image');
-    img.src=linkCard;
-    div.classList.add('card__footer');
-    h2.classList.add('card__title');
-    h2.textContent=nameCard;
-    buttonLike.classList.add('card__like', 'link');
-    buttonLike.ariaLabel='liked';
-    buttonLike.type='button';
-
-    div.append(h2, buttonLike);
-    article.append(img, div);
-    li.append(article);
-    return li; 
-
-
-
+    return `<li class="card">
+        <article>
+            <img class="card__image" src="${linkCard}">
+            <div class="card__footer">
+                <h2 class="card__title">${nameCard}</h2>
+                <button class="card__like link" type="button" aria-label="liked"></button>
+            </div>
+        </article>
+    </li>`
 }
 
-cardsList.append(...initialCards.map((cardItem) => {
-    return createCard(cardItem.name, cardItem.link);
-}))
+const renderCardList = (cardName, cardLink) =>{
+    cardsList.insertAdjacentHTML('afterbegin', createCard(cardName, cardLink));
+}
+
+initialCards.forEach((cardItem) => {
+    renderCardList(cardItem.name, cardItem.link);
+});
+
+// cardsList.insertAdjacentElement('beforebegin',  map(createCard).join('\n'));
