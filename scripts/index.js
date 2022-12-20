c262391fd831f57fe9b49af973b7eab0311049b0
Lvova Alina <lvova.aline@gmail.com>
2022-12-20 16:08:52 +0400

    feature: первый способ добавления элементов

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,34 @@ const popupAddForm = document.querySelector('#popup-add-form');
 const popupCloseBtnEditForm = document.querySelector('#close-btn-edit');
 const popupCloseBtnAddForm = document.querySelector("#close-btn-add");
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+
 let titleInput = document.querySelector('#input-title');
 let linkInput = document.querySelector('#input-link');
 
@@ -29,7 +57,7 @@ const openPopupEditForm = () => {
 
 editButton.addEventListener('click', openPopupEditForm);
 
-addButton.addEventListener('click', () => {openPopup(popupAddForm);});
+addButton.addEventListener('click',  () => { openPopup(popupAddForm);});
 
 popupCloseBtnEditForm.addEventListener('click', () => {
     closePopup(popupEditForm);
@@ -47,11 +75,47 @@ function saveInputEditForm (evt) {
 }
 
 function saveInputAddForm (evt) {
-    evt.preventDefault();    
-    descrptProfile.textContent = titleInput.value; 
-    nameProfile.textContent =  linkInput.value; 
-    closePopup(popupAddForm); 
+    evt.preventDefault();
+    // descrptProfile.textContent = titleInput.value; 
+    // nameProfile.textContent =  linkInput.value; 
+    closePopup(popupAddForm);
+    titleInput.value="";
+    linkInput.value="";
 }
 
 popupEditForm.addEventListener('submit', saveInputEditForm);
 popupAddForm.addEventListener('submit', saveInputAddForm);
+
+
+const cardsList = document.querySelector('.gallery__list');
+
+const createCard = (nameCard, linkCard) => {
+    const li = document.createElement('li');
+    const article = document.createElement('article');
+    const img =  document.createElement('img');
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const buttonLike = document.createElement('button');
+
+    li.classList.add('card');
+    img.classList.add('card__image');
+    img.src=linkCard;
+    div.classList.add('card__footer');
+    h2.classList.add('card__title');
+    h2.textContent=nameCard;
+    buttonLike.classList.add('card__like', 'link');
+    buttonLike.ariaLabel='liked';
+    buttonLike.type='button';
+
+    div.append(h2, buttonLike);
+    article.append(img, div);
+    li.append(article);
+    return li; 
+
+
+
+}
+
+cardsList.append(...initialCards.map((cardItem) => {
+    return createCard(cardItem.name, cardItem.link);
+}))
