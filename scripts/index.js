b7f4feec73082c74295b00c4553dc12093d64bce
Lvova Alina <lvova.aline@gmail.com>
2022-12-20 14:45:51 +0400

    refactor:

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,13 @@
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
-let popupEditForm = document.querySelector('#popup-edit-form');
-let popupAddForm = document.querySelector('#popup-add-form');
+const popupEditForm = document.querySelector('#popup-edit-form');
+const popupAddForm = document.querySelector('#popup-add-form');
 const popupCloseBtnEditForm = document.querySelector('#close-btn-edit');
 const popupCloseBtnAddForm = document.querySelector("#close-btn-add");
 
 let titleInput = document.querySelector('#input-title');
 let linkInput = document.querySelector('#input-link');
-// let titleImg = document.querySelector('')
 
-// let EditForm = document.querySelector('.popup__edit-form');
 let nameInput = document.querySelector('#input-name');
 let descrptInput = document.querySelector('#input-descrpt');
 let nameProfile = document.querySelector('.profile__name');
@@ -19,34 +17,19 @@ const closePopup = (popup) => {
     popup.classList.remove('popup_opened');
   };
 
-function saveInputEditForm (evt) {
-    evt.preventDefault();    
-    descrptProfile.textContent = descrptInput.value;
-    nameProfile.textContent = nameInput.value; 
-    closePopup(popupEditForm); 
-}
-
-
-function saveInputAddForm (evt) {
-    evt.preventDefault();    
-    descrptProfile.textContent = titleInput.value; 
-    nameProfile.textContent =  linkInput.value; 
-    closePopup(popupAddForm); 
-}
+const openPopup = (popup) => {
+    popup.classList.add('popup_opened');
+};
 
 const openPopupEditForm = () => {
     descrptInput.value = descrptProfile.textContent;
     nameInput.value = nameProfile.textContent;
-    let temp = document.querySelector('#popup-edit-form');
-    popupEditForm.classList.add("popup_opened");
-    console.log(temp);
+    openPopup(popupEditForm);
 };
 
 editButton.addEventListener('click', openPopupEditForm);
 
-addButton.addEventListener('click', () =>{
-     popupAddForm.classList.add('popup_opened');
-});
+addButton.addEventListener('click', () => {openPopup(popupAddForm);});
 
 popupCloseBtnEditForm.addEventListener('click', () => {
     closePopup(popupEditForm);
@@ -56,7 +39,19 @@ popupCloseBtnAddForm.addEventListener('click', () => {
     closePopup(popupAddForm);   
 });
 
+function saveInputEditForm (evt) {
+    evt.preventDefault();    
+    descrptProfile.textContent = descrptInput.value;
+    nameProfile.textContent = nameInput.value; 
+    closePopup(popupEditForm); 
+}
+
+function saveInputAddForm (evt) {
+    evt.preventDefault();    
+    descrptProfile.textContent = titleInput.value; 
+    nameProfile.textContent =  linkInput.value; 
+    closePopup(popupAddForm); 
+}
+
 popupEditForm.addEventListener('submit', saveInputEditForm);
 popupAddForm.addEventListener('submit', saveInputAddForm);
-
-console.log(popupEditForm, popupAddForm);
