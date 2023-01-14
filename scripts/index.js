85ba2b8029742bfa77478503f9eed61e99e121de
Lvova Alina <lvova.aline@gmail.com>
2023-01-14 15:10:13 +0400

    fix: bugfix

--- scripts/index.js
+++ scripts/index.js
@@ -66,38 +66,25 @@ function saveProfile(evt) {
   closePopup(popupProfileForm);
 }
 
-function saveNewCard (evt) {
+function saveNewCard(evt) {
   evt.preventDefault();
   renderCardList(titleImgInput.value, linkImgInput.value);
   closePopup(popupAddForm);
   evt.target.reset();
-}
-
-//удалить обработчик событий для кнопки закрытия
-function removeEventListenerButtonClose (popup){
-  const closeButton = popup.querySelector('.popup__close-btn');
-  closeButton.removeEventListener('click', () => closePopup(popup));
-}
-
-//добавить обработчик для закрытия по overlay
-function removeEventListenerOverlayClose(popup) {
-  const popupContainer = popup.querySelector('.overlay');
-  popupContainer.removeEventListener('click', (evt) => { closePopupIfEventContains(evt, 'overlay', popup) });
-  popup.removeEventListener('click', (evt) => { closePopupIfEventContains(evt, 'popup_opened', popup) });
+  evt.submitter.classList.add(validationConfig.inactiveButtonClass);
+  evt.submitter.disabled = true;
 }
 
 const closePopup = (popup) => {
   document.removeEventListener('keydown', closePopupByEscape);
   popup.classList.remove('popup_opened');
-  removeEventListenerButtonClose(popup);
-  removeEventListenerOverlayClose(popup);
 };
 
 const closePopupByEscape = (evt) => {
-    if (evt.key === 'Escape') {
-      const popup = popupList.find(popupElem => popupElem.classList.contains('popup_opened'));
-      closePopup(popup);
-    }
+  if (evt.key === 'Escape') {
+    const popup = document.querySelector('.popup_opened');
+    closePopup(popup);
+  }
 }
 
 function openProfilePopup() {
@@ -106,13 +93,7 @@ function openProfilePopup() {
   openPopup(popupProfileForm);
 }
 
-function openAddPopup() {
-  titleImgInput.value = '';
-  linkImgInput.value = '';
-  openPopup(popupAddForm);
-}
-
-function addEventListenerButtonClose (popup){
+function addEventListenerButtonClose(popup) {
   const closeButton = popup.querySelector('.popup__close-btn');
   // устанавливаем обработчик закрытия на крестик 
   closeButton.addEventListener('click', () => closePopup(popup));
@@ -120,7 +101,7 @@ function addEventListenerButtonClose (popup){
 
 //закрыть попап, если событие содержит класс
 const closePopupIfEventContains = (evt, _class, popup) => {
-  if (evt.target.classList.contains(_class)){
+  if (evt.target.classList.contains(_class)) {
     closePopup(popup);
   }
 };
@@ -133,11 +114,8 @@ function addEventListenerOverlayClose(popup) {
 }
 
 const openPopup = (popup) => {
-  enableValidation(validationConfig);
   document.addEventListener('keydown', closePopupByEscape);
   popup.classList.add('popup_opened');
-  addEventListenerButtonClose(popup);
-  addEventListenerOverlayClose(popup);
 };
 
 const createCard = (nameCard, linkCard) => {
@@ -161,7 +139,7 @@ const openPopupImgForm = (nameCard, linkCard) => {
   figcaptionCard.textContent = nameCard;
   imgPopup.src = linkCard;
   imgPopup.alt = nameCard;
-  openPopup(popupImgForm); 
+  openPopup(popupImgForm);
 }
 
 const renderCardList = (cardName, cardLink) => {
@@ -175,4 +153,11 @@ initialCards.forEach((cardItem) => {
 popupProfileForm.addEventListener('submit', saveProfile);
 popupAddForm.addEventListener('submit', saveNewCard);
 editButton.addEventListener('click', () => { openProfilePopup() });
-addButton.addEventListener('click', () => { openAddPopup() });
+addButton.addEventListener('click', () => { openPopup(popupAddForm) });
+
+enableValidation(validationConfig);
+
+popupList.forEach(popup => {
+  addEventListenerButtonClose(popup);
+  addEventListenerOverlayClose(popup);
+});
